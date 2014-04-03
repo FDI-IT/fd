98698ddbaf0cb685ea872cbb5c3ca4a16d1ad9fd
Steve Stachurski <steves@flavordynamics.com>
2014-04-03 10:29:07 -0400

    Made experimental log table sortable

--- staticmedia/js/reports_experimental_log.js
+++ staticmedia/js/reports_experimental_log.js
@@ -1,5 +1,54 @@
 
 jQuery(document).ready(function(){	
+	$( "#reports_experimental_log_details" ).tablesorter({
+			headers: {
+			0: {
+				sorter: 'text'
+			},
+			1: {
+				sorter: 'link-digit'
+			},
+			2: {
+				sorter: 'link-digit'
+			},
+			3: {
+				sorter: 'text'
+			},
+			4: {
+				sorter: 'text'
+			},
+			5: {
+				sorter: 'shortDate'
+			},
+			6: {
+				sorter: 'link-digit'
+			},
+			7: {
+				sorter: 'digit'
+			},
+			8: {
+				sorter: 'digit'
+			},
+			9: {
+				sorter: 'digit'
+			},
+			10: {
+				sorter: 'digit'
+			},
+			11: {
+				sorter: 'digit'
+			},
+			12: {
+				sorter: 'digit'
+			},
+			13: {
+				sorter: 'digit'
+			},
+			14: {
+				sorter: 'digit'
+			}
+	}});
+	
 	jQuery("#reports_experimental_log_details :checkbox").change( function(){
 		var my_tr = jQuery(this).closest('tr');
 		var my_checked = this.checked;
