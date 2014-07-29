837ece76211d8ab5b1597577668788c060f5dd63
Steve Stachurski <steves@flavordynamics.com>
2014-07-29 16:13:54 -0400

    Fixed settings.py

--- settings.py
+++ settings.py
@@ -73,7 +73,7 @@ MEDIA_URL = '/djangomedia/'
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.load_template_source',
+    'django.template.loaders.eggs.Loader',
 )
 
 
